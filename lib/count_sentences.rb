cb734ddaa5d0768596456ce26db37e3d65a52a39
Ian Cossentino <icossentino@gmail.com>
2020-11-12 17:28:39 -0500

    commit tree 520276224e454f6b1eaf03e6d752ed8eb1a9217f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
+
+    self[-1] == "?" ? true : false
 
   end
 
   def exclamation?
 
+    self[-1] == "!" ? true : false
+
   end
 
   def count_sentences
-
+    sent_array = self.split(/[.?!]/)
+    sent_array.delete_if {|word| word == ""}
+    sent_array.length
   end
 end
+
+complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+complex_string.count_sentences
+
+
